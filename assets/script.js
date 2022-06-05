a2c4e69c1b7bc8a6bb4b699059ffc3031ac894e8
Alex <alexander.h.perry@gmail.com>
2022-06-06 00:14:33 +1000

    Score object added, Scores save to local storage - TODO: Show High Scores + timer

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-
 var startQuiz = document.getElementById("start-quiz");
 var quizquestion = document.getElementById("quizquestion"); //this is the h1 element
 var quizanswer = document.getElementById("quizanswer");
-var q=0; // Question Number count
+var q = 0; // Question Number count
 var score = 0; // Start with a score of zero, +5 per correct answer
+var HighScores = JSON.parse(localStorage.getItem("HighScores"));
 
 // Quiz Questions - array item stores the data for each question
 var questions = [
@@ -51,53 +51,79 @@ var questions = [
 
 // Display the questions where q is the index for the question number. 
 // Create button for the answer of each question. 
-function ShowQuestion(q){
-    console.log("q is:", q)
-    if(q > 3){
-        alert("Your Score is: "+ score)
-        //go to finished function... 
+function ShowQuestion(q) {
+    if (q >= 4) { //Object.keys(questions).length
+        DisplayScore();
         return
     };
     quizquestion.textContent = questions[q].question;
     quizanswer.textContent = "";
     // the question[].answer is not a zero-based array, this is referencing the key pair in the object. 
-    for (let index = 1; index < Object.keys(questions[q].answer).length+1; index++) { 
-       console.log(questions[q].answer[index]);
-       var ansButton = document.createElement("button"); //create the answer buttons. 
-       ansButton.setAttribute("data-index", index);
-       ansButton.textContent =  index + ". "+  questions[q].answer[index];
-       quizanswer.appendChild(ansButton);
-    }
-}
-// Event for the "Start Quiz" being clicked. (Start the quiz, and hide itself)
-startQuiz.addEventListener('click', function() {
+    for (let index = 1; index < Object.keys(questions[q].answer).length + 1; index++) {
+        var ansButton = document.createElement("button"); //create the answer buttons. 
+        ansButton.setAttribute("data-index", index);
+        ansButton.textContent = index + ". " + questions[q].answer[index];
+        quizanswer.appendChild(ansButton);
+    };
+};
+
+// Event for the "Start Quiz" being clicked.
+// Resets question count and score to 0, hides the startQuiz button and starts the quiz. 
+startQuiz.addEventListener('click', function () {
     q = 0; // reset the question count to 0
-    ShowQuestion(q)    
-    this.style.display = "none";  
+    score = 0;
+    this.style.display = "none";
+    ShowQuestion(q)
 });
-// Event for the answer button being clicked. Listens on each child 'answer' button. q is the question number as defined above. 
-quizanswer.addEventListener('click', function(event) {
-    if(q > 3){
+
+// Event for Answer buttons being pressed. Matches up if the selected answer is the correct answer listed in the object. 
+// If correct, adds a point otherwise removes 10 seconds from the time. 
+quizanswer.addEventListener('click', function (event) {
+    if (q >= Object.keys(questions).length) {
         return
     };
     var element = event.target;
     var dataIndex = element.getAttribute("data-index");
-    console.log(element.getAttribute("data-index"));
-    if(dataIndex == questions[q].correctAnswer){
-        console.log("correct");
-        score = score + 5;
-    }
-    else {
-        console.log(q, dataIndex)
-        // timer minus 10 seconds . 
+    if (dataIndex == questions[q].correctAnswer) {
+        score++;
     };
+    // Remove 10 seconds from the time
     q++;
     ShowQuestion(q);
-})
+});
 
-function DisplayScore(){
+//Display final score, allow player to save high score, go to high score page on save, or restart. 
+function DisplayScore() {
     quizquestion.textContent = "Well Done!";
-    quizanswer.textContent = "Your Final Score is: ", score;
-    // create form with input box and submit button. 
-    startQuiz.style.display = block;
+    quizanswer.textContent = "Your Final Score is: " + score + "/" + Object.keys(questions).length; //Display the player's final score (with how many were correct)
+
+    // create form with input box and submit button.
+    var hsInitials = document.createElement("input");
+    hsInitials.setAttribute("placeholder", "Enter your Initials");
+    var hsSubmit = document.createElement("button");
+    hsSubmit.textContent = "Submit";
+    quizanswer.appendChild(hsInitials);
+    quizanswer.appendChild(hsSubmit);
+
+    // Event on button click to save high scores
+    hsSubmit.addEventListener("click", function () {
+        var initials = hsInitials.value;
+        if (HighScores == null) {
+            HighScores = {};
+        };
+        HighScores[initials] = score;
+        localStorage.setItem("HighScores", JSON.stringify(HighScores));
+        // Create High Score Function
+        ShowHighScores();
+    }
+    );
+
+    function ShowHighScores(){
+        quizquestion.textContent = "High Scores";
+        quizanswer.textContent = "Scorez";
+        // Need to load in the HighScores object. 
+    }
+    //----
+    startQuiz.style.display = "block";
+    return;
 }
